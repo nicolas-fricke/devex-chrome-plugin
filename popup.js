28d9d918552a5453283763c3157da1672fd64625
Nicolas Fricke <nicolas.fricke@devex.com>
2015-06-12 19:11:12 +0200

    Add PRs on watchlist to popup

--- popup.js
+++ popup.js
@@ -19,3 +19,19 @@ $(document).on('settingsLoaded', function(){
 function buildGithubApiQuery(route, githubToken) {
   return "https://api.github.com" + route + "?access_token=" + githubToken
 }
+
+$(document).on('watchlistLoaded', function() {
+  $.each(storageManager.watchlist, function(index, watchedElement) {
+    var $newRow = $('#pr-template-row')
+      .clone()
+      .appendTo('#watchlist-body')
+      .attr('data-index', index);
+    $newRow.children('.repository').children('a')
+      .text(watchedElement.repository)
+      .attr('href', 'https://github.com/' + watchedElement.repository);
+    $newRow.children('.pr-name').children('a')
+      .text(watchedElement.pullRequestName)
+      .attr('href', 'https://github.com/' + watchedElement.repository + '/pull/' + watchedElement.pullRequestNumber.slice(1));
+    $newRow.removeClass('hidden');
+  });
+});
